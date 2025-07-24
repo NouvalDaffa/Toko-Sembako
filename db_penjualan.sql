f8ab6f0e8d9f775bfe475e484f12ef251b24e583
NouvalDaffa <nouvaldaffa54@gmail.com>
2025-07-24 10:00:37 +0700

    first commit

---
+++ db_penjualan.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2024 at 06:25 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `kode_barang` varchar(10) DEFAULT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `harga_beli` varchar(100) DEFAULT NULL,
+  `harga_jual` varchar(100) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `satuan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `harga_beli`, `harga_jual`, `stok`, `satuan`) VALUES
+(1, '53525585', 'Telur', '10000', '12500', 8, 'kg'),
+(2, '90598509', 'Kopi Kapal Api', '1500', '1750', 14, 'sachet'),
+(4, '33602675', 'Gula Pasir', '7500', '8000', 14, 'kg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_toko`
+--
+
+CREATE TABLE `data_toko` (
+  `id` int(11) NOT NULL,
+  `nama_toko` varchar(80) DEFAULT NULL,
+  `nama_pemilik` varchar(80) DEFAULT NULL,
+  `no_telepon` varchar(15) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_toko`
+--
+
+INSERT INTO `data_toko` (`id`, `nama_toko`, `nama_pemilik`, `no_telepon`, `alamat`) VALUES
+(1, 'Toko Sembako', 'Banyak', '141251531', 'Depok');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `no_penjualan` varchar(20) DEFAULT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `harga_barang` varchar(20) DEFAULT NULL,
+  `jumlah_barang` int(11) DEFAULT NULL,
+  `satuan` varchar(20) DEFAULT NULL,
+  `sub_total` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_penjualan`
+--
+
+INSERT INTO `detail_penjualan` (`no_penjualan`, `nama_barang`, `harga_barang`, `jumlah_barang`, `satuan`, `sub_total`) VALUES
+('PJ1732339419', 'Telur', '12500', 2, 'kg', '25000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasir`
+--
+
+CREATE TABLE `kasir` (
+  `id` int(11) NOT NULL,
+  `kode_kasir` varchar(10) DEFAULT NULL,
+  `nama_kasir` varchar(100) DEFAULT NULL,
+  `username_kasir` varchar(20) DEFAULT NULL,
+  `password_kasir` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kasir`
+--
+
+INSERT INTO `kasir` (`id`, `kode_kasir`, `nama_kasir`, `username_kasir`, `password_kasir`) VALUES
+(3, 'KASIR - 65', 'jamal', 'KSR65', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id` int(11) NOT NULL,
+  `kode_pengguna` varchar(10) DEFAULT NULL,
+  `nama_pengguna` varchar(100) DEFAULT NULL,
+  `username_pengguna` varchar(20) DEFAULT NULL,
+  `password_pengguna` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `kode_pengguna`, `nama_pengguna`, `username_pengguna`, `password_pengguna`) VALUES
+(2, 'gnt', 'genta', 'genta', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id` int(11) NOT NULL,
+  `no_penjualan` varchar(20) DEFAULT NULL,
+  `nama_kasir` varchar(100) DEFAULT NULL,
+  `tgl_penjualan` varchar(20) DEFAULT NULL,
+  `jam_penjualan` varchar(20) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id`, `no_penjualan`, `nama_kasir`, `tgl_penjualan`, `jam_penjualan`, `total`) VALUES
+(4, 'PJ1732339419', 'jamal', '23/11/2024', '12:23:39', 25000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `data_toko`
+--
+ALTER TABLE `data_toko`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kasir`
+--
+ALTER TABLE `kasir`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `data_toko`
+--
+ALTER TABLE `data_toko`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kasir`
+--
+ALTER TABLE `kasir`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
